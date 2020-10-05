fd1ff17ea252f2517ebebe4c228fdab4cf56c97a
Sean Cahill <sean.j.cahill@gmail.com>
2020-10-05 10:02:17 -0400

    📝 Added and Updated Content

--- _build/jupyter_execute/content/timeseries.py
+++ _build/jupyter_execute/content/timeseries.py
@@ -39,7 +39,10 @@ print(str(stamp))
 
 stamp.strftime('%Y-%m')
 
-strftime allows you to select certain parts of the datetime object with using the special formatting codes. Here is a list of common ones:
+(strf)=
+### Strftime
+
+**strftime** allows you to select certain parts of the datetime object with using the special formatting codes. Here is a list of common ones: (ISO C89 compatible)
 
 - `%Y` : 4 digit year
 - `%y` : 2 digit year
@@ -53,4 +56,77 @@ strftime allows you to select certain parts of the datetime object with using th
 - `%F` : shortcut for `%Y-%m-%d`
 - `%D` : shortcut for `%d/%m/%y`
 
+
+Here is a list of useful locale-specific **strftime** codes:
+
+- `%a` : abbreviated day name
+- `%A` : Full-length day name
+- `%b` : abbreviated month name
+- `%B` : Full month name
+- `%c` : full date-time (Tue 01 May 2012 04:20:57 PM’)
+- `%p` : locale equivalent of a.m./p.m.
+- `%x` : locale-appropriate formatted date (%c)
+- `%X` : locale-appropriate time
+
+(datetime64)=
+## Numpy [datetime64](https://numpy.org/doc/stable/reference/arrays.datetime.html)
+
+Like most native python objects and and processes - problems with efficiency and speed arise when dealing with large quantities and loops. 
+
+The dypte **datetime64** was developed in order to address these problems.
+
+The `datetime64` datatype encodes dates as 64 bit integers (which allows dates to be represented compactly)
+
+import numpy as np
+date = np.array('2020-07-04', dtype='datetime64[D]')
+date
+
+Dates in this format allow for vectorized operations. 
+````{margin}
+```{note}
+Recall that alot of the speed attainable in `numpy` is due to the uniformity 
+of dtype in numpy arrays
+```
+````
+
+date + np.arange(6)
+
+`datetime64` and `timedelta` are both built upon a **fundamental time unit** and so the range of encodable dates = 
+
+$$ 2^{64} \cdot (t_{u}) $$
+
+where $t_{u}$ is the smallest unit of time measured **(time resolution)** and the product above is the **max time span**  
+
+$ns = 10^{-9}$ (nanoseconds) are the default ftu. but there are quite a few codes that can be used.
+
+np.datetime64('2020-08-11', 'ns')
+
+Here are the various codes for `datetime64`:  
+
+- `Y` : year
+- `M` : month
+- `W` : ...
+- `D` : 
+- `h` : 
+- `m` :
+- `s` :
+- `ms` : millisecond
+- `us` : microsecond $10^{-6}$ one millionth of a second
+- `ns` : nanosecond
+- `ps` : picosecond $10^{-12}$ one trillionth of a second
+- `fs` : femtosecond
+- `as` : attosecond $10^{-18}$ one quintillionth of a second
+
+(pandasdate)=
+## Dates and Times in Pandas
+
+Pandas builds upon `datetime`, `dateutils`, and `datetime64` to provide objects such as `Timestamp` that takes the best part of each:  
+
+The flexibility and ease of use of native `python3` packages, and the efficient storage and vectorized interface of `numpy`
+
+import pandas as pd
+date = pd.to_datetime('11 of August, 2020')
+date
+
+date.strftime("%A")
 
